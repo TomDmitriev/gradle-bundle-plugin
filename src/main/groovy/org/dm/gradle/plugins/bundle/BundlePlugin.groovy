e896a1948e164aa35f3ef449da5037e44210ff87
Raymond Auge <raymond.auge@liferay.com>
2019-04-28 20:43:21 -0400

    DeleteAllActions is deprecated
    fixes #78
    
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- src/main/groovy/org/dm/gradle/plugins/bundle/BundlePlugin.groovy
+++ src/main/groovy/org/dm/gradle/plugins/bundle/BundlePlugin.groovy
@@ -29,7 +29,7 @@ class BundlePlugin implements Plugin<Project> {
                 def jarBuilderFactory = new JarBuilderFactoryDecorator(
                         jarTask, project.bundle.jarBuilderFactory)
 
-                deleteAllActions()
+                setActions([])
                 doLast(new BundleGenerator(jarBuilderFactory))
                 manifest = new ManifestSubstitute(jarBuilderFactory, manifest)
             }
